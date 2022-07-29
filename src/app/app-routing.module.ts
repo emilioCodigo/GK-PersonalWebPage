629b377b1d33db18b241e4d1c60ea35e7e59df8c
Hex-Zhou <zywzjsc@gmail.com>
2022-07-29 19:40:57 +0800

    refactor: kill some route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
-import { titleList } from './constant/const/title.const'
-import { Error404Component } from './pages/core/error404/error404.component'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
-import { title } from 'process'
+import { titleList } from './constant/const/title.const'
+import { Error404Component } from './pages/core/error404/error404.component'
 
 const routes: Routes = [
     {
@@ -10,21 +9,7 @@ const routes: Routes = [
         title: titleList.root + '首頁',
         loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule),
     },
-    {
-        path: 'search',
-        title: titleList.root + '搜尋',
-        loadChildren: () => import('./pages/search/search.module').then((m) => m.SearchModule),
-    },
-    {
-        path: 'product',
-        title: titleList.root + '產品',
-        loadChildren: () => import('./pages/product/product.module').then((m) => m.ProductModule),
-    },
-    {
-        path: 'member',
-        title: titleList.root + '會員',
-        loadChildren: () => import('./pages/member/member.module').then((m) => m.MemberModule),
-    },
+
     { path: '404', component: Error404Component, title: '找不到網頁' },
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: '**', pathMatch: 'full', redirectTo: '404' },
